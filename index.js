b43e3cf97af48ed7a4e293c5b63ff0217c977408
Jebosa Samson <Jebosasamson7@gmail.com>
2024-04-13 15:08:55 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// query the relevant html items
+// Add relevant event listener
+// Add Increment and decrement function
+
+
+let button = document.querySelector('.button')
+let value = document.querySelector('.number')
+
+const numberChange = function(){}
+
+button.addEventListener('click',function()){
+
+}
